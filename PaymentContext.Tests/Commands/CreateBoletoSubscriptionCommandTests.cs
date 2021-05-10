738733992ef54bebfdbf4fd19f502c728a0d4325
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-05-10 11:47:38 -0300

    Adiciona commands e exemplo de teste

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestClass]
+        public class DocumentTests
+        {
+            [TestMethod]
+            public void ShouldReturnErrorWhenNameIsInvalid()
+            {
+                var command = new CreateBoletoSubscriptionCommand();
+                command.FirstName = "";
+                command.Validate();
+                Assert.AreEqual(false, command.IsValid);
+            }
+        }
+    }
+}
