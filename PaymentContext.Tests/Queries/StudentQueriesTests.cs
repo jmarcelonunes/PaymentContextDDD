68afec5bd8493be54ccf43debc7413fe90804558
Joao Marcelo Nunes Chaves <jmarcelonunes738@gmail.com>
2021-05-10 14:27:08 -0300

    Implementa queries e testes.

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Queries;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        public StudentQueriesTests()
+        {
+            _students = new List<Student>();
+            for (var i = 0; i <= 10; i++)
+            {
+                _students.Add(new Student(
+                    new Name("Aluno", i.ToString()),
+                    new Document("11111111111" + i.ToString(), EDocumentType.CPF),
+                    new Email(i.ToString() + "@email.com")
+                    ));
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNotExists()
+        {
+            var exp = StudentQueries.GetStudent("99999999999");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, student);
+        }
+
+        [TestMethod]
+        public void ShouldReturnStudentWhenDocumentExists()
+        {
+            var exp = StudentQueries.GetStudent("11111111111");
+            var student = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, student);
+        }
+    }
+}
